a3197852b506e2b6030e108b332e10acf0c6c12e
mirqarp1z <orucaydin13@gmail.com>
2025-07-25 11:15:25 +0000

    task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints a specific sentence using puts
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
