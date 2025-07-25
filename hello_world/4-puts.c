b6a5d08b2b4145f82e26541e1cab4c123076baac
mirqarp1z <orucaydin13@gmail.com>
2025-07-25 11:03:05 +0000

    task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\"");
-    return 0;
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-
