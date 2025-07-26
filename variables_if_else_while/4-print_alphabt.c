1ea241690a52a926562ac78cc25dfeddc80e590c
root <orucaydin13@gmail.com>
2025-07-26 17:39:33 +0000

    task

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch == 'e') || (ch == 'q'))
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+}
