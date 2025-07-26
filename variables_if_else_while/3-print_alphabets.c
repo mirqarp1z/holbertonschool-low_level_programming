d72cbf14ad79097dd8e8dea6444f0555220c7006
root <orucaydin13@gmail.com>
2025-07-26 16:49:51 +0000

    task

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch_low;
+	char ch_up;
+
+	ch_low = 'a';
+
+	while (ch_low <= 'z')
+	{
+		putchar(ch_low);
+		ch_low++;
+	}
+	while (ch_up <= 'Z')
+	{
+		putchar(ch_up);
+		ch_up++;
+	putchar('\n');
+	return (0);
+}
