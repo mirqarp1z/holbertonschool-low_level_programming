edee132f4a79f1127aa4f5f4cdcb356ade4ab0f3
root <orucaydin13@gmail.com>
2025-07-27 11:43:29 +0000

    task

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char ch;
+
+	a = 0;
+
+	ch = a;
+
+	while (x <= 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
