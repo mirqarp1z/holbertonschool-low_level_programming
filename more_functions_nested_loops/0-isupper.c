cf9bb95ebdcab50d594a8f7ba5a961daef249cbb
root <orucaydin13@gmail.com>
2025-07-28 09:56:01 +0000

    task

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _isupper - checks if the letter is upper.
+ *
+ * @c - letter.
+ *
+ * Return - 1 if letter is upper, otherwise 0.
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
