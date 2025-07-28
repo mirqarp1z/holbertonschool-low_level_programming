799866deaec6f22572c3657bee6f803a8f622031
root <orucaydin13@gmail.com>
2025-07-28 10:56:47 +0000

    task

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14,
+ * each followed by a new line.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int lines = 1;
+
+	int digit = 0;
+
+	while (lines <= 10)
+	{
+		while ( digit <= 14)
+		{
+			_putchar(dg + '0');
+			dg++;
+		}
+		lines++;
+	}
+	_putchar('\n');
+}
