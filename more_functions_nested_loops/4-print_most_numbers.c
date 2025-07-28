1bc657dba8d638fe2e717d2725f962cab639edf5
root <orucaydin13@gmail.com>
2025-07-28 10:36:46 +0000

    task

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 except 2 and 4,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int dg = 0;
+
+	while (dg <= 9)
+	{
+		if ((dg == 2) || (dg == 4))
+		{
+			ch++;
+			continue;
+		}
+		else
+		{
+			_putchar(dg + '0');
+		}
+		_putchar(\n);
+	}
+}
