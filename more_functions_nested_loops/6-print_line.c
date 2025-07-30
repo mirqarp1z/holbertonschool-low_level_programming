77e4cf15af6b517c5392e318ea18c2facfd4def5
root <orucaydin13@gmail.com>
2025-07-30 09:04:58 +0000

    task

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line of underscores followed by a newline
+ * @n: number of underscores to print
+ */
+
+
+void print_line(int n)
+{
+	if ((n == 0) || (n < 0))
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+	}
+	_putchar('\n');
+}
