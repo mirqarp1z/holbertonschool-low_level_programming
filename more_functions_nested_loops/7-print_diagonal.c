7b1c6b6c9314f65376473c532ac81a8f8abe1e43
root <orucaydin13@gmail.com>
2025-07-30 09:16:08 +0000

    task

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line of backslashes followed by a newline
+ * @n: number of backslashes to print
+ */
+
+void print_diagonal(int n)
+{
+	if ((n == 0) || (n > 0))
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_puthcar('\\');
+			n--;
+		}
+		_putchar('\n');
+	}
+}
