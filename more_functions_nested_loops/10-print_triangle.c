0c52fa1814da2e8d116b7713e7c794429d7833f9
root <orucaydin13@gmail.com>
2025-07-30 13:16:39 +0000

    task

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of '#' characters
+ * @size: size of the triangle
+ *
+ * If size is 0 or less, prints only a newline.
+ */
+
+void print_triangle(int size)
+{}
