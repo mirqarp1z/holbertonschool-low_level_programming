6267756d47955c963c70f663f9186f034e691f1a
root <orucaydin13@gmail.com>
2025-08-05 11:12:05 +0000

    task

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with the byte b
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory with
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
