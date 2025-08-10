59e7ae3fd6a1da93ab378a2c6e24bda2e4c06e24
mirqarp1z <orucaydin13@gmail.com>
2025-08-10 18:39:22 +0000

    task

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen_recursion - Returns the length of a string using recursion
@@ -9,5 +10,7 @@
 
 int _strlen_recursion(char *s)
 {
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
-
