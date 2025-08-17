ae61402d3983bc2a56ae20ceca7d3d8271dc6892
mirqarp1z <orucaydin13@gmail.com>
2025-08-17 19:13:48 +0000

    task

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * _strdup - Duplicates a string.
  * @str: The string to be duplicated.
@@ -9,35 +9,60 @@
  *         string str. Returns NULL if str is NULL or if memory allocation
  *         fails.
  *
- * Description: This function allocates memory to store a duplicate of the
- *              input string and returns a pointer to the new memory space.
- *              The allocated memory can later be freed using the free()
- *              function. If str is NULL or if memory allocation fails, the
- *              function returns NULL.
+ * This function allocates memory to store a duplicate of the input string 
+ * and returns a pointer to the new memory space. The allocated memory can 
+ * later be freed using the free() function. If str is NULL or if memory 
+ * allocation fails, the function returns NULL. The length of the string is 
+ * calculated using the custom my_length function, which manually counts the 
+ * characters of the string until the null terminator is reached.
  */
 
 char *_strdup(char *str)
 {
-	char *cp;
-	unsigned int ln;
-	unsigned int i;
+    char *cp;
+    unsigned int ln;
+    unsigned int i;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+    if (str == NULL)
+    {
+        return (NULL);
+    }
 
-	ln = strlen(str);
+    ln = my_length(str) + 1;
 
-	cp = malloc(ln * sizeof(char));
+    cp = malloc(ln * sizeof(char));
 
-	if (cp == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i <= ln; i++)
-	{
-		cp[i]=str[i];
-	}
-	return (cp);
+    if (cp == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < ln; i++)
+    {
+        cp[i] = str[i];
+    }
+
+    return (cp);
 }
+
+/**
+ * my_length - Calculates the length of a string.
+ * @s: The string to measure.
+ *
+ * Return: The length of the string (excluding the null terminator).
+ *
+ * 
+ */
+
+unsigned int my_length(char *s)
+{
+    unsigned int len = 0;
+
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+
+    return (len);
+}
+
