9940a8e89498f7717d165cce371dbe52f5fb206d
mirqarp1z <orucaydin13@gmail.com>
2025-08-17 19:31:07 +0000

    task

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,55 +2,44 @@
 #include <stdlib.h>
 
 /**
- * my_length - Calculates the length of a string.
- * @s: The string to measure.
+ * my_length - Returns the length of a string
+ * @s: The string to measure
  *
- * Return: The length of the string.
+ * Return: The length of the string
  */
-
-unsigned int my_length(char *s);
-
-/**
- * _strdup - Duplicates a string.
- * @str: The string to duplicate.
- *
- * Return: A pointer to the newly allocated memory, or NULL if failed.
- */
-char *_strdup(char *str)
-{
-	char *cp;
-	unsigned int ln;
-	unsigned int i;
-	
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	
-	ln = my_length(str) + 1;
-	
-	cp = malloc(ln * sizeof(char));
-	
-	if (cp == NULL)
-	{
-		return (NULL);
-	}
-	
-	for (i = 0; i < ln; i++)
-	{
-		cp[i] = str[i];
-	}
-	
-	return (cp);
-}
-
 unsigned int my_length(char *s)
 {
 	unsigned int len = 0;
-	
+
 	while (s[len] != '\0')
-	{
 		len++;
-	}
+
 	return (len);
+}
+
+/**
+ * _strdup - Creates a duplicate of a string
+ * @str: The string to duplicate
+ *
+ * Return: Pointer to the new string, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	unsigned int i;
+	unsigned int len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = my_length(str) + 1;
+
+	cpy = malloc(len * sizeof(char));
+	if (cpy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		cpy[i] = str[i];
+
+	return (cpy);
 }
