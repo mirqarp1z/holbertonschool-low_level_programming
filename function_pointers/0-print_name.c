fc523a865640f88bb9c27ccee193e79171c5cf1b
mirqarp1z <orucaydin13@gmail.com>
2025-08-20 12:57:10 +0000

    task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - Prints a name using a given function pointer
+ * @name: The name to be printed
+ * @f: A pointer to a function that takes a char* and returns void
+ *
+ * Description: This function prints the provided name by calling the
+ * function pointed to by 'f'. If 'name' or 'f' is NULL, the function
+ * does nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+}
