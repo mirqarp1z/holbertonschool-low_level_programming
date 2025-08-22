2bfda3deaad56518ee518e61bacf194f34fd6900
mirqarp1z <orucaydin13@gmail.com>
2025-08-22 15:06:07 +0000

    task

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -15,12 +15,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if ((array == NULL) || (action == NULL))
 	{
 		return;
 	}
 
-	for (int i = 0; i < size; i++ )
+	for (i = 0; i < size; i++ )
 	{
 		action(array[i]);
 	}
